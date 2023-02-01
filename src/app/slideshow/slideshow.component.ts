0d8485d501b5378d3c6646c31a492eeae49bf4c1
Marco von Baumbach <marco.vonbaumbach@gmx.de>
2023-02-01 08:37:04 +0100

    header

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit{
+  title = 'angular';
+  images = ['1.jpg', '2.jpg', '3.jpg'];
+  headlines = ['Bring Engineering To The Next Level', 'Born To Code', 'Graduated At TUM'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;  
+      }, 10);
+    }, 5000)
+  }
+}
